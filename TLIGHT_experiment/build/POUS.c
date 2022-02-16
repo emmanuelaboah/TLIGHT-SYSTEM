378ba7d762abaff9ac01de3a5dc2af0a8076098e
Emmanuel Aboah Boateng <aboahemmanuel01@yahoo.com>
2022-02-15 22:17:27 -0600

    Build file of the experimental setup

---
+++ TLIGHT_experiment/build/POUS.c
@@ -0,0 +1,296 @@
+void LOGGER_init__(LOGGER *data__, BOOL retain) {
+  __INIT_VAR(data__->EN,__BOOL_LITERAL(TRUE),retain)
+  __INIT_VAR(data__->ENO,__BOOL_LITERAL(TRUE),retain)
+  __INIT_VAR(data__->TRIG,__BOOL_LITERAL(FALSE),retain)
+  __INIT_VAR(data__->MSG,__STRING_LITERAL(0,""),retain)
+  __INIT_VAR(data__->LEVEL,LOGLEVEL__INFO,retain)
+  __INIT_VAR(data__->TRIG0,__BOOL_LITERAL(FALSE),retain)
+}
+
+// Code part
+void LOGGER_body__(LOGGER *data__) {
+  // Control execution
+  if (!__GET_VAR(data__->EN)) {
+    __SET_VAR(data__->,ENO,,__BOOL_LITERAL(FALSE));
+    return;
+  }
+  else {
+    __SET_VAR(data__->,ENO,,__BOOL_LITERAL(TRUE));
+  }
+  // Initialise TEMP variables
+
+  if ((__GET_VAR(data__->TRIG,) && !(__GET_VAR(data__->TRIG0,)))) {
+    #define GetFbVar(var,...) __GET_VAR(data__->var,__VA_ARGS__)
+    #define SetFbVar(var,val,...) __SET_VAR(data__->,var,__VA_ARGS__,val)
+
+   LogMessage(GetFbVar(LEVEL),(char*)GetFbVar(MSG, .body),GetFbVar(MSG, .len));
+  
+    #undef GetFbVar
+    #undef SetFbVar
+;
+  };
+  __SET_VAR(data__->,TRIG0,,__GET_VAR(data__->TRIG,));
+
+  goto __end;
+
+__end:
+  return;
+} // LOGGER_body__() 
+
+
+
+
+
+void PYTHON_EVAL_init__(PYTHON_EVAL *data__, BOOL retain) {
+  __INIT_VAR(data__->EN,__BOOL_LITERAL(TRUE),retain)
+  __INIT_VAR(data__->ENO,__BOOL_LITERAL(TRUE),retain)
+  __INIT_VAR(data__->TRIG,__BOOL_LITERAL(FALSE),retain)
+  __INIT_VAR(data__->CODE,__STRING_LITERAL(0,""),retain)
+  __INIT_VAR(data__->ACK,__BOOL_LITERAL(FALSE),retain)
+  __INIT_VAR(data__->RESULT,__STRING_LITERAL(0,""),retain)
+  __INIT_VAR(data__->STATE,0,retain)
+  __INIT_VAR(data__->BUFFER,__STRING_LITERAL(0,""),retain)
+  __INIT_VAR(data__->PREBUFFER,__STRING_LITERAL(0,""),retain)
+  __INIT_VAR(data__->TRIGM1,__BOOL_LITERAL(FALSE),retain)
+  __INIT_VAR(data__->TRIGGED,__BOOL_LITERAL(FALSE),retain)
+}
+
+// Code part
+void PYTHON_EVAL_body__(PYTHON_EVAL *data__) {
+  // Control execution
+  if (!__GET_VAR(data__->EN)) {
+    __SET_VAR(data__->,ENO,,__BOOL_LITERAL(FALSE));
+    return;
+  }
+  else {
+    __SET_VAR(data__->,ENO,,__BOOL_LITERAL(TRUE));
+  }
+  // Initialise TEMP variables
+
+  __IL_DEFVAR_T __IL_DEFVAR;
+  __IL_DEFVAR_T __IL_DEFVAR_BACK;
+  #define GetFbVar(var,...) __GET_VAR(data__->var,__VA_ARGS__)
+  #define SetFbVar(var,val,...) __SET_VAR(data__->,var,__VA_ARGS__,val)
+extern void __PythonEvalFB(int, PYTHON_EVAL*);__PythonEvalFB(0, data__);
+  #undef GetFbVar
+  #undef SetFbVar
+;
+
+  goto __end;
+
+__end:
+  return;
+} // PYTHON_EVAL_body__() 
+
+
+
+
+
+void PYTHON_POLL_init__(PYTHON_POLL *data__, BOOL retain) {
+  __INIT_VAR(data__->EN,__BOOL_LITERAL(TRUE),retain)
+  __INIT_VAR(data__->ENO,__BOOL_LITERAL(TRUE),retain)
+  __INIT_VAR(data__->TRIG,__BOOL_LITERAL(FALSE),retain)
+  __INIT_VAR(data__->CODE,__STRING_LITERAL(0,""),retain)
+  __INIT_VAR(data__->ACK,__BOOL_LITERAL(FALSE),retain)
+  __INIT_VAR(data__->RESULT,__STRING_LITERAL(0,""),retain)
+  __INIT_VAR(data__->STATE,0,retain)
+  __INIT_VAR(data__->BUFFER,__STRING_LITERAL(0,""),retain)
+  __INIT_VAR(data__->PREBUFFER,__STRING_LITERAL(0,""),retain)
+  __INIT_VAR(data__->TRIGM1,__BOOL_LITERAL(FALSE),retain)
+  __INIT_VAR(data__->TRIGGED,__BOOL_LITERAL(FALSE),retain)
+}
+
+// Code part
+void PYTHON_POLL_body__(PYTHON_POLL *data__) {
+  // Control execution
+  if (!__GET_VAR(data__->EN)) {
+    __SET_VAR(data__->,ENO,,__BOOL_LITERAL(FALSE));
+    return;
+  }
+  else {
+    __SET_VAR(data__->,ENO,,__BOOL_LITERAL(TRUE));
+  }
+  // Initialise TEMP variables
+
+  __IL_DEFVAR_T __IL_DEFVAR;
+  __IL_DEFVAR_T __IL_DEFVAR_BACK;
+  #define GetFbVar(var,...) __GET_VAR(data__->var,__VA_ARGS__)
+  #define SetFbVar(var,val,...) __SET_VAR(data__->,var,__VA_ARGS__,val)
+extern void __PythonEvalFB(int, PYTHON_EVAL*);__PythonEvalFB(1,(PYTHON_EVAL*)(void*)data__);
+  #undef GetFbVar
+  #undef SetFbVar
+;
+
+  goto __end;
+
+__end:
+  return;
+} // PYTHON_POLL_body__() 
+
+
+
+
+
+void PYTHON_GEAR_init__(PYTHON_GEAR *data__, BOOL retain) {
+  __INIT_VAR(data__->EN,__BOOL_LITERAL(TRUE),retain)
+  __INIT_VAR(data__->ENO,__BOOL_LITERAL(TRUE),retain)
+  __INIT_VAR(data__->N,0,retain)
+  __INIT_VAR(data__->TRIG,__BOOL_LITERAL(FALSE),retain)
+  __INIT_VAR(data__->CODE,__STRING_LITERAL(0,""),retain)
+  __INIT_VAR(data__->ACK,__BOOL_LITERAL(FALSE),retain)
+  __INIT_VAR(data__->RESULT,__STRING_LITERAL(0,""),retain)
+  PYTHON_EVAL_init__(&data__->PY_EVAL,retain);
+  __INIT_VAR(data__->COUNTER,0,retain)
+  __INIT_VAR(data__->ADD10_OUT,0,retain)
+  __INIT_VAR(data__->EQ13_OUT,__BOOL_LITERAL(FALSE),retain)
+  __INIT_VAR(data__->SEL15_OUT,0,retain)
+  __INIT_VAR(data__->AND7_OUT,__BOOL_LITERAL(FALSE),retain)
+}
+
+// Code part
+void PYTHON_GEAR_body__(PYTHON_GEAR *data__) {
+  // Control execution
+  if (!__GET_VAR(data__->EN)) {
+    __SET_VAR(data__->,ENO,,__BOOL_LITERAL(FALSE));
+    return;
+  }
+  else {
+    __SET_VAR(data__->,ENO,,__BOOL_LITERAL(TRUE));
+  }
+  // Initialise TEMP variables
+
+  __SET_VAR(data__->,ADD10_OUT,,ADD__UINT__UINT(
+    (BOOL)__BOOL_LITERAL(TRUE),
+    NULL,
+    (UINT)2,
+    (UINT)__GET_VAR(data__->COUNTER,),
+    (UINT)1));
+  __SET_VAR(data__->,EQ13_OUT,,EQ__BOOL__UINT(
+    (BOOL)__BOOL_LITERAL(TRUE),
+    NULL,
+    (UINT)2,
+    (UINT)__GET_VAR(data__->N,),
+    (UINT)__GET_VAR(data__->ADD10_OUT,)));
+  __SET_VAR(data__->,SEL15_OUT,,SEL__UINT__BOOL__UINT__UINT(
+    (BOOL)__BOOL_LITERAL(TRUE),
+    NULL,
+    (BOOL)__GET_VAR(data__->EQ13_OUT,),
+    (UINT)__GET_VAR(data__->ADD10_OUT,),
+    (UINT)0));
+  __SET_VAR(data__->,COUNTER,,__GET_VAR(data__->SEL15_OUT,));
+  __SET_VAR(data__->,AND7_OUT,,AND__BOOL__BOOL(
+    (BOOL)__BOOL_LITERAL(TRUE),
+    NULL,
+    (UINT)2,
+    (BOOL)__GET_VAR(data__->EQ13_OUT,),
+    (BOOL)__GET_VAR(data__->TRIG,)));
+  __SET_VAR(data__->PY_EVAL.,TRIG,,__GET_VAR(data__->AND7_OUT,));
+  __SET_VAR(data__->PY_EVAL.,CODE,,__GET_VAR(data__->CODE,));
+  PYTHON_EVAL_body__(&data__->PY_EVAL);
+  __SET_VAR(data__->,ACK,,__GET_VAR(data__->PY_EVAL.ACK,));
+  __SET_VAR(data__->,RESULT,,__GET_VAR(data__->PY_EVAL.RESULT,));
+
+  goto __end;
+
+__end:
+  return;
+} // PYTHON_GEAR_body__() 
+
+
+
+
+
+void PROGRAM0_init__(PROGRAM0 *data__, BOOL retain) {
+  __INIT_VAR(data__->P_RED,__BOOL_LITERAL(FALSE),retain)
+  __INIT_VAR(data__->P_GREEN,__BOOL_LITERAL(FALSE),retain)
+  __INIT_VAR(data__->V_RED,__BOOL_LITERAL(FALSE),retain)
+  __INIT_VAR(data__->V_YEL,__BOOL_LITERAL(FALSE),retain)
+  __INIT_VAR(data__->V_GREEN,__BOOL_LITERAL(FALSE),retain)
+  __INIT_VAR(data__->P_RIGHT,__BOOL_LITERAL(FALSE),retain)
+  __INIT_VAR(data__->P_LEFT,__BOOL_LITERAL(FALSE),retain)
+  __INIT_VAR(data__->START,__BOOL_LITERAL(FALSE),retain)
+  __INIT_VAR(data__->T2,__BOOL_LITERAL(FALSE),retain)
+  __INIT_VAR(data__->T3,__BOOL_LITERAL(FALSE),retain)
+  __INIT_VAR(data__->T4,__BOOL_LITERAL(FALSE),retain)
+  __INIT_VAR(data__->T5,__BOOL_LITERAL(FALSE),retain)
+  __INIT_VAR(data__->T6,__BOOL_LITERAL(FALSE),retain)
+  TON_init__(&data__->TON0,retain);
+  TON_init__(&data__->TON1,retain);
+  TON_init__(&data__->TON2,retain);
+  TON_init__(&data__->TON3,retain);
+  __INIT_VAR(data__->C1,0,retain)
+  __INIT_VAR(data__->C2,0,retain)
+  __INIT_VAR(data__->C3,0,retain)
+  __INIT_VAR(data__->C4,0,retain)
+  __INIT_VAR(data__->C5,0,retain)
+  TON_init__(&data__->TON4,retain);
+  TON_init__(&data__->TON5,retain);
+  __INIT_VAR(data__->TIME_TO_INT61_OUT,0,retain)
+  __INIT_VAR(data__->TIME_TO_INT42_OUT,0,retain)
+  __INIT_VAR(data__->TIME_TO_INT63_OUT,0,retain)
+  __INIT_VAR(data__->TIME_TO_INT35_OUT,0,retain)
+}
+
+// Code part
+void PROGRAM0_body__(PROGRAM0 *data__) {
+  // Initialise TEMP variables
+
+  __SET_VAR(data__->,START,,(!(__GET_VAR(data__->P_RIGHT,)) && (!(__GET_VAR(data__->P_LEFT,)) || __GET_VAR(data__->START,))));
+  __SET_VAR(data__->,V_GREEN,,((!(__GET_VAR(data__->V_RED,)) && !(__GET_VAR(data__->V_YEL,))) && __GET_VAR(data__->START,)));
+  __SET_VAR(data__->TON1.,IN,,(!(__GET_VAR(data__->T4,)) && __GET_VAR(data__->START,)));
+  __SET_VAR(data__->TON1.,PT,,__time_to_timespec(1, 0, 60, 0, 0, 0));
+  TON_body__(&data__->TON1);
+  __SET_VAR(data__->,V_YEL,,(!(__GET_VAR(data__->V_RED,)) && __GET_VAR(data__->TON1.Q,)));
+  __SET_VAR(data__->TON0.,IN,,(!(__GET_VAR(data__->T4,)) && __GET_VAR(data__->START,)));
+  __SET_VAR(data__->TON0.,PT,,__time_to_timespec(1, 0, 64, 0, 0, 0));
+  TON_body__(&data__->TON0);
+  __SET_VAR(data__->,V_RED,,__GET_VAR(data__->TON0.Q,));
+  __SET_VAR(data__->,P_RED,,((!(__GET_VAR(data__->V_RED,)) || __GET_VAR(data__->T2,)) && __GET_VAR(data__->START,)));
+  __SET_VAR(data__->,P_GREEN,,(!(__GET_VAR(data__->P_RED,)) && __GET_VAR(data__->START,)));
+  __SET_VAR(data__->TON2.,IN,,__GET_VAR(data__->P_GREEN,));
+  __SET_VAR(data__->TON2.,PT,,__time_to_timespec(1, 0, 10, 0, 0, 0));
+  TON_body__(&data__->TON2);
+  __SET_VAR(data__->,T2,,((!(__GET_VAR(data__->T4,)) && __GET_VAR(data__->T2,)) || __GET_VAR(data__->TON2.Q,)));
+  __SET_VAR(data__->,TIME_TO_INT61_OUT,,TIME_TO_INT(
+    (BOOL)__BOOL_LITERAL(TRUE),
+    NULL,
+    (TIME)__GET_VAR(data__->TON2.ET,)));
+  __SET_VAR(data__->,C3,,__GET_VAR(data__->TIME_TO_INT61_OUT,));
+  __SET_VAR(data__->TON4.,IN,,__GET_VAR(data__->V_YEL,));
+  __SET_VAR(data__->TON4.,PT,,__time_to_timespec(1, 0, 3, 0, 0, 0));
+  TON_body__(&data__->TON4);
+  __SET_VAR(data__->,TIME_TO_INT42_OUT,,TIME_TO_INT(
+    (BOOL)__BOOL_LITERAL(TRUE),
+    NULL,
+    (TIME)__GET_VAR(data__->TON4.ET,)));
+  __SET_VAR(data__->,C1,,__GET_VAR(data__->TIME_TO_INT42_OUT,));
+  __SET_VAR(data__->,C2,,__GET_VAR(data__->TIME_TO_INT42_OUT,));
+  __SET_VAR(data__->TON3.,IN,,__GET_VAR(data__->T2,));
+  __SET_VAR(data__->TON3.,PT,,__time_to_timespec(1, 0, 6, 0, 0, 0));
+  TON_body__(&data__->TON3);
+  __SET_VAR(data__->,T3,,(((!(__GET_VAR(data__->T4,)) && __GET_VAR(data__->V_RED,)) && __GET_VAR(data__->T3,)) || __GET_VAR(data__->TON3.Q,)));
+  __SET_VAR(data__->,TIME_TO_INT63_OUT,,TIME_TO_INT(
+    (BOOL)__BOOL_LITERAL(TRUE),
+    NULL,
+    (TIME)__GET_VAR(data__->TON3.ET,)));
+  __SET_VAR(data__->,C4,,__GET_VAR(data__->TIME_TO_INT63_OUT,));
+  __SET_VAR(data__->TON5.,IN,,__GET_VAR(data__->T3,));
+  __SET_VAR(data__->TON5.,PT,,__time_to_timespec(1, 0, 2, 0, 0, 0));
+  TON_body__(&data__->TON5);
+  __SET_VAR(data__->,T4,,__GET_VAR(data__->TON5.Q,));
+  __SET_VAR(data__->,TIME_TO_INT35_OUT,,TIME_TO_INT(
+    (BOOL)__BOOL_LITERAL(TRUE),
+    NULL,
+    (TIME)__GET_VAR(data__->TON5.ET,)));
+  __SET_VAR(data__->,C5,,__GET_VAR(data__->TIME_TO_INT35_OUT,));
+
+  goto __end;
+
+__end:
+  return;
+} // PROGRAM0_body__() 
+
+
+
+
+
